a3d594cd796a7ee5d8ad5af0d478a87eb39970c9
rogerioduenas <rogerioduenasdev@hotmail.com>
2024-12-06 14:41:48 +0900

    feat: create responsive header

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import HeaderMenu from "@/components/HeaderMenu";
 
 function Container() {
   const theme = useTheme();
 
   return (
-    <Typography
-      variant='h1ExtraBold'
-      color='primary'
-      sx={{
-        height: '100px',
-        width: '100px',
-        background: theme.gradient.primary,
-      }}
-    >
-      Container
-    </Typography>
+    <Box>
+      <HeaderMenu/>
+    </Box>
   );
 }
 
