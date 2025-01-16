1058292bc1814cbd2d12b666bb3524b555d22787
rogerioduenas <rogerioduenasdev@hotmail.com>
2025-01-16 14:33:37 +0900

    feat: trailer section

---
+++ src/components/Community/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './style.module.scss';
+import { Box, Typography } from '@mui/material';
+
+function Community() {
+  return (
+    <Box className={styles.container}>
+      <Typography>huasuhuahsuhasuhas</Typography>
+      <Typography>huasuhuahsuhasuhas</Typography>
+      <Typography>huasuhuahsuhasuhas</Typography>
+      <Typography>huasuhuahsuhasuhas</Typography>
+      <Typography>huasuhuahsuhasuhas</Typography>
+      <Typography>huasuhuahsuhasuhas</Typography>
+      <Typography>huasuhuahsuhasuhas</Typography>
+    </Box>
+  )
+}
+
+export default Community;
--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import styles from './style.module.scss'
 import { Box } from '@mui/material';
 import HeaderMenu from '@/components/HeaderMenu';
 import Hero from '@/components/Hero';
 import Features from '@/components/Features';
-import CarouselCatalog from '@/components/CarouselCatalog'
+import CarouselCatalog from '@/components/CarouselCatalog';
+import Trailers from '@/components/Trailers';
+import Community from '@/components/Community';
 
 function Container() {
   return (
-    <Box className={styles.container}>
+    <Box>
       <HeaderMenu />
       <Hero />
       <Features />
       <CarouselCatalog />
+      <Trailers/>
+      <Community/>
     </Box>
   );
 }
